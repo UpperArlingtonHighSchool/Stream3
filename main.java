e66378dc0ddc37cea85c754b087f76a0b8e66b71
ehowee <105238598+ehowee@users.noreply.github.com>
2022-05-23 14:07:54 -0400

    Update and rename main to main.java

--- main.java
+++ main.java
@@ -8,7 +8,8 @@ LeData default = new LeData();
 ArrayList <LeHardness> uaData = default.LeMethod();
 
 //call Jim's data method
-
+WebScraper jData = new Webscraper();
+ArrayList <Integer> jData = jData.hardness();
 
 //input data into Ceci's charts
 
