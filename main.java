f5da92c56089a4e85410d22d501f6ccd7f6c1b7d
ehowee <105238598+ehowee@users.noreply.github.com>
2022-05-24 12:06:52 -0400

    Update main.java

--- main.java
+++ main.java
@@ -13,7 +13,25 @@ ArrayList <Integer> jData = jData.hardness();
 
 //input data into Ceci's charts
 
-//run GUI to display charts, data, and stats
+//run GUI to display charts and data
+ JFrame frame = new JFrame("Input Dialog");
 
+ //for loop to display TJ's data
+  uaString = "";
+ for(int x = 0, x < uaData.length() ,x++)
+ {
+  uaString += uaData.get(x).toString()) + "\n";
+ }
+  JOptionPane.showMessageDialog(frame, "Hardness and pH data from UA streams\n" + uaString);
+  
 }
+  
+ //for loop to display Jim's data
+  jString = "";
+  for(int y = 0, y < jData.length(), y++)
+  {
+   jString += jData.get(x).hardness() + " ";
+  }
+  
+   JOptionPane.showMessageDialog(frame, "Hardness data from Ohio streams\n" + jString);
 }
